2520b872672a631dd6d188ffaffcf6a6b161b860
saul bard <saulbard41@gmail.com>
2021-08-31 21:54:38 +1000

    get, update and delete routes for reminders

--- db/reminders.ts
+++ db/reminders.ts
@@ -23,10 +23,63 @@ const createReminder = (reminder: Reminder) : Promise<any> => {
     });
 }
 
+const updateReminder = (reminder: Reminder) : Promise<any> => {
+    return new Promise((resolve, reject) => {
+        pool.query("UPDATE reminders SET title = $1, description = $2, due_date = $3, recurring = $4, recurring_timeframe = $5, remind_date_time = $6, phone_id = $7, updated_at = NOW() WHERE external_ref = $8 RETURNING *",
+        [reminder.title, reminder.description, reminder.dueDate, reminder.recurring, reminder.recurringTimeframe, reminder.remindDateTime, reminder.phoneId, reminder.externalRef], (error, results) => {
+            if (error) {
+                reject(new Error(error.message));
+            }
+
+            resolve( results.rows[0])
+        })
+    });
+}
+
+const getReminderByExternalRef = (reminderExternalRef: string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        pool.query("SELECT * FROM reminders where external_ref = $1", [reminderExternalRef], (err, results) => {
+            if (err) {
+                reject( new Error(err.message));
+            }
+
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getRemindersForUser = (userExternalRef: string) : Promise<any> => {
+    return new Promise((resolve, reject) => {
+        pool.query("SELECT * FROM reminders WHERE user_id = $1", [userExternalRef], (error, results) => {
+            if (error) {
+                reject(new Error(error.message));
+            }
+
+            resolve(results.rows);
+        });
+    });
+}
+
+const deleteReminder = (reminderExternalRef: string) : Promise<any> => {
+    return new Promise((resolve, reject) => {
+        pool.query("DELETE FROM reminders WHERE external_ref = $1 RETURNING *",  [reminderExternalRef], (err, results) => {
+            if (err) {
+                reject(new Error(err.message));
+            }
+
+            resolve(results.rows[0]);
+        })
+    })
+}
+
 
 
 const remindersDb = {
     createReminder,
+    updateReminder,
+    getRemindersForUser,
+    deleteReminder,
+    getReminderByExternalRef
 }
 
 export default remindersDb;
--- routes/reminders.ts
+++ routes/reminders.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response} from 'express';
 import { Reminder } from '../models/reminder';
 import remindersDb from "../db/reminders";
+import { ObjectUtil } from '../utils/objectUtil';
+import user from '../db/user';
 const verifyToken = require("../middleware/jwt");
 const router = express.Router();
 
@@ -33,22 +35,94 @@ router.post("/", verifyToken, async (req: Request, res: Response) => {
 
 // update reminder
 router.put("/", verifyToken, async (req: Request, res: Response) => {
+    try {
+        let reminder = new Reminder (req.body);
+        const user = req.body.user;
 
+        if (user.externalRef !== reminder.userId) {
+            throw new Error("Unauthorized");
+        }
+    
+        let updatedReminder = await remindersDb.updateReminder(reminder);
+    
+        if (!updatedReminder) {
+            throw new Error ("Unable to update reminder");
+        }
+    
+        reminder = Reminder.dloToDto(updatedReminder);
+    
+        res.status(200).send(reminder);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
 });
 
 // delete reminder
 router.delete("/:id", verifyToken, async (req: Request, res: Response) => {
 
+    try {
+        let reminder = await remindersDb.getReminderByExternalRef(req.params.id);
+        const user = req.body.user;
+        if (!reminder) {
+            throw new Error ("Unable to Find Reminder");
+        }
+
+        let reminderDto = Reminder.dloToDto(reminder);
+
+        if (reminderDto.userId !== user.externalRef) {
+            throw new Error ("Unauthorized");
+        }
+
+        let deletedReminder = await remindersDb.deleteReminder(req.params.id);
+        if (!deletedReminder) {
+            throw new Error("Unable to delete reminder");
+        }
+
+        res.status(200).send(reminderDto);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 // get reminder by id
 router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+    try {
+        let reminder = await remindersDb.getReminderByExternalRef(req.params.id);
+        const user = req.body.user;
+        if (!reminder) {
+            throw new Error ("Unable to Find Reminder");
+        }
 
+        let reminderDto:Reminder = Reminder.dloToDto(reminder);
+        if (reminderDto.userId !== user.externalRef) {
+            throw new Error ("Unauthorized");
+        }
+
+        res.status(200).send(reminderDto);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 // get all reminders by user
 router.get("/", verifyToken, async (req: Request, res: Response) => {
+    try {
+        const user = req.body.user;
 
+        let reminders:any[] = await remindersDb.getRemindersForUser(user.externalRef);
+
+        if (!reminders) {
+            throw new Error("Unable to retrieve reminders");
+        }
+
+        let rems: Reminder[] = reminders.map((r) => Reminder.dloToDto(r));
+
+        res.status(200).send(rems);
+
+
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
 });
 
 
